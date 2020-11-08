ad505aaa77ebbbd6066f56a8ef29160c2a6ab851
Jitendra Pal <55188975+threej-in@users.noreply.github.com>
2020-11-08 16:52:48 +0530

    main configuration file

---
+++ includes/config_3j.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Telegram bot token should be replaced with your own bots unique token which
+ * you can get from bot father.
+ * 
+ * @link to botfather http://t.me/botfather
+ */
+define('BOT_TOKEN', '');
+
+/**
+ * Set your unique chat id if you want to receive logs/notification from the bot
+ * when something odd happens or when a new user starts your bot.
+ * 
+ * You can get your chat id form [@forwardinfobot](http://t.me/forwardinfobot")
+ */
+define('ADMINID','');
+
+/**
+ * Database configuration
+ */
+
+/**
+ * set your database server name if you are unaware about your db server name then
+ * you can get it from your hosting provider.
+ */
+define('DBSERVER', '');
+/**
+ * set your database user name you can get it from your hosting provider.
+ */
+define('DBUSERNAME', '');
+/**
+ * set your database password you can get it from your hosting provider.
+ */
+define('DBPASSWORD', '');
+/**
+ * set your database name.
+ */
+define('DBNAME', '');
